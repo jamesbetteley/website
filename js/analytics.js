1432e5e01b73f391bd0918336593dfe7a8b39e63
Edmund Dipple <elmundio1987@gmail.com>
2015-02-21 21:08:27 +0000

    analytics code

---
+++ js/analytics.js
@@ -0,0 +1,21 @@
+var livedomain = "devopsninjas.azurewebsites.net";
+
+if(window.location.host == livedomain) {
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-60001958-1', 'auto');
+    ga('send', 'pageview');
+}
+else{
+    console.log('Not the main website ${livedomain}, skipping analytics');
+}
